42b557aed8a6b5019142832d497c5962b888a3fa
Georgi Mollov <georgi.mollov@flolive.net>
2021-06-27 11:47:50 +0300

    Add house details component

---
+++ src/app/houses/house/house.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HouseComponent } from './house.component';
+
+
+
+@NgModule({
+  declarations: [
+    HouseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HouseModule { }
