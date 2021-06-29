90e71ee682ab09feabb075a0a589034c60eb38d1
Georgi Mollov <georgi.mollov@flolive.net>
2021-06-29 10:11:01 +0300

    Share replay to avoid double request and use it like a cache

--- src/app/shared/services/books.service.ts
+++ src/app/shared/services/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Book} from '../interfaces/book';
+import {shareReplay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,8 @@ export class BooksService {
   }
 
   getBooks(url: string): Observable<Book[]>{
-    return this.httpSvc.get<Book[]>(url)
+    return this.httpSvc.get<Book[]>(url).pipe(
+      shareReplay(1)
+    )
   }
 }
